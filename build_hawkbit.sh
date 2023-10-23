13230d45112728de55dbc075b1d7dd0cecef6f97
HuyK <huy.nguyen@optisigns.com>
2023-10-23 14:54:37 +0700

    Update build script to support DevOps deployment work

--- build_hawkbit.sh
+++ build_hawkbit.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+rm -rf dist
+mkdir dist
 mvn clean install -DskipTests
+mv ./hawkbit-runtime/hawkbit-update-server/target/hawkbit-update-server-0.3.0-SNAPSHOT.jar dist/hawkbit-update-server.jar
